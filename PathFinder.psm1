7491a46bf3b2d8205ae676be7c96fcd99263c453
drlsdee <tracert0@gmail.com>
2020-07-24 17:10:46 +0500

    The module is divided into separate scripts. Added some basic tests. The minimum PowerShell version is set to "5.1". Needs assembly "System.Linq.Enumerable".

--- PathFinder.psm1
+++ PathFinder.psm1
@@ -1,239 +1,98 @@
-function Get-Path {
-    <#
-    .SYNOPSIS
-        The function converts string with delimiter (e.g. environment variable $env:Path) to array of strings.
-    .DESCRIPTION
-        The function converts string with delimiter (e.g. environment variable $env:Path) to array of strings.
-    .EXAMPLE
-        PS C:\> Get-Path -Path $env:Path
-        Converts environment variable $env:Path to array of strings. Flag "OnlyAvailable" is set by default, so the function will return only existing paths.
-    .EXAMPLE
-        PS C:\> Get-Path -Path $env:Path -OnlyAvailable:$false
-        Converts environment variable $env:Path to array of strings. Flag "OnlyAvailable" is unset, so the function will return all paths.
-    .INPUTS
-        [System.String]
-    .OUTPUTS
-        [System.Array]
-    .NOTES
-        ...
-    #>
+
+function Get-PSScriptsInFolder {
     [CmdletBinding()]
     param (
-        # Source path from $env:Path, $env:PSModulePath or from elsewhere like $env:SMS_ADMIN_UI_PATH. Default is $env:Path.
-        [Parameter(HelpMessage="Source path from env:Path, env:PSModulePath or from elsewhere like env:SMS_ADMIN_UI_PATH. Default is env:Path.")]
+        # Folder to search
+        [Parameter()]
         [string]
-        $Path = $env:Path,
+        $Path,
 
-        # Returns only available paths if checked. Default value is "True".
-        [Parameter(HelpMessage="Returns only available paths if checked. Default value is True.")]
-        [bool]
-        $OnlyAvailable = $true
-    )
-
-    begin {
-        [array]$pathToArray = $Path.Split(";").Where({$_.Length -ne 0})
-        [array]$outArray = @()
-    }
-
-    process {
-        if ($OnlyAvailable) {
-            [array]$tempArray = @()
-            foreach ($pathStr in $pathToArray) {
-                if (Test-Path -Path $pathStr) {
-                    $tempArray += $pathStr
-                }
-            }
-            $outArray = $tempArray
-        } else {
-            $outArray = $pathToArray
-        }
-    }
-
-    end {
-        return $outArray
-    }
-}
-
-function Find-Path {
-    <#
-    .SYNOPSIS
-        The function finds path by keyword.
-    .DESCRIPTION
-        The function finds path by keyword.
-    .EXAMPLE
-        PS C:\> Find-Path -Keyword putty -Path $env:Path
-        Returns from environment variable $env:Path path with keyword "putty":
-        PS C:\> C:\Program Files\PuTTY\
-    .EXAMPLE
-        PS C:\> Find-Path -Keyword pageant -Path $env:Path -InContent
-        Function will get paths from $env:Path and will search for files with keyword "pageant", then will return paths to folders where the files are present:
-        PS C:\> C:\Program Files\PuTTY\
-        PS C:\> C:\Program Files\Git\cmd
-    .INPUTS
-        [System.String]
-    .OUTPUTS
-        [System.Array]
-    .NOTES
-        ...
-    #>
-    [CmdletBinding()]
-    param (
-        # Search string
-        [Parameter(Mandatory=$true,HelpMessage="Keyword for search, mandatory parameter")]
-        [string]
-        $Keyword,
-
-        # Path to search, default is $env:Path
-        [Parameter(HelpMessage="Path to search, default is env:Path")]
-        [string]
-        $Path = $env:Path,
-
-        # If true, will search for matches in filenames in path folders. Default is "False".
-        [Parameter(HelpMessage="If true, will search for matches in filenames in path folders. Default is False.")]
+        # Include files with double extensions, e.g. '*.tests.ps1', '*.build.ps1' etc.
+        [Parameter()]
         [switch]
-        $InContent = $false
+        $IncludeTests
     )
-    
-    begin {
-        [array]$tempArray = Get-Path -Path $Path
-        [array]$outArray = @()
+    [string]$myName                     =   "$($MyInvocation.InvocationName):"
+
+    if (-not [System.IO.Directory]::Exists($Path)) {
+        Write-Verbose -Message "$myName Directory `"$Path`" does not exist! Exiting."
+        return
     }
-    
-    process {
-        foreach ($pathStr in $tempArray) {
-            if (!$InContent) {
-                if ($pathStr -match $Keyword) {
-                    $outArray += $pathStr
-                }
-            } else {
-                if ((Get-ChildItem -Path $pathStr -File).Name -match $Keyword) {
-                    $outArray += $pathStr
-                }
-            }
-        }
+
+    [System.IO.FileInfo[]]$psScriptsAll =   [System.IO.Directory]::EnumerateFiles($Path, '*.ps1')
+    if ($psScriptsAll.Count -eq 0) {
+        Write-Verbose -Message "$myName Directory `"$Path`" does not contain PowerShell scripts! Exiting."
+        return
     }
-    
-    end {
-        return $outArray
+    Write-Verbose -Message "$myName Directory `"$Path`" contains $($psScriptsAll.Count) PowerShell scripts total."
+
+    if ($IncludeTests) {
+        Write-Verbose -Message "$myName Returning all $($psScriptsAll.Count) PowerShell script paths found in folder `"$Path`" including tests, build scripts etc."
+        [string[]]$psScriptsOut         =   $psScriptsAll.FullName
+        return    $psScriptsOut
     }
+
+    [string[]]$psScriptsOut             =   $psScriptsAll.Where({
+        -not ([regex]::IsMatch($_.BaseName, '\.'))
+    }).FullName
+
+    if ($psScriptsOut.Count -eq 0) {
+        Write-Verbose -Message "$myName Seems like the folder `"$Path`" contains only tests, build scripts or something like. Nothing to return!"
+        return
+    }
+    Write-Verbose -Message "$myName Found $($psScriptsAll.Count) PowerShell scripts in folder `"$Path`"."
+    return      $psScriptsOut
 }
 
-function Add-Path {
-    <#
-    .SYNOPSIS
-        Adds path to environment. Returns string.
-    .DESCRIPTION
-        Adds path to environment. Returns string.
-    .EXAMPLE
-        PS C:\> Add-Path -Path "C:\Path\Does\Not\Exists" -Scope $env:Path
-        Adds path "C:\Path\Does\Not\Exists" to environment variable $env:Path, if path "C:\Path\Does\Not\Exists" exists.
-    .EXAMPLE
-        PS C:\> Add-Path -Path "C:\Path\Does\Not\Exists" -Scope $env:Path -OnlyIfExists:$false
-        Adds path "C:\Path\Does\Not\Exists" to environment variable $env:Path, even if path "C:\Path\Does\Not\Exists" does NOT exists.
-    .INPUTS
-        [System.String]
-    .OUTPUTS
-        [System.String]
-    .NOTES
-        ...
-    #>
-    [CmdletBinding()]
-    param (
-        # Path to add
-        [Parameter(Mandatory=$true,HelpMessage="Path to add")]
-        [string]
-        $Path,
+[string]$fldrNameClasses    =   [System.IO.Path]::Combine($PSScriptRoot, 'Classes')
+[string]$fldrNameFuncPriv   =   [System.IO.Path]::Combine($PSScriptRoot, 'Functions', 'Private')
+[string]$fldrNameFuncPubl   =   [System.IO.Path]::Combine($PSScriptRoot, 'Functions', 'Public')
 
-        # Scope. Default is $env:Path.
-        [Parameter(HelpMessage="Scope. Default is env:Path.")]
-        [string]
-        $Scope = $env.Path,
+[string[]]$importClasses    =   Get-PSScriptsInFolder -Path $fldrNameClasses
+[string[]]$importFuncPriv   =   Get-PSScriptsInFolder -Path $fldrNameFuncPriv
+[string[]]$importFuncPubl   =   Get-PSScriptsInFolder -Path $fldrNameFuncPubl
+[string[]]$exportFunctions  =   $importFuncPubl.ForEach({
+    [System.IO.Path]::GetFileNameWithoutExtension($_)
+})
 
-        # Test path before addition. Default is $true.
-        [Parameter(HelpMessage="Test path before addition. Default is True.")]
-        [bool]
-        $OnlyIfExists = $true
-    )
-    
-    begin {
-        [array]$tempArray = Get-Path -Path $Scope
-        [string]$outString = $null
+$importClasses.ForEach({
+    [string]$scriptName =   $_
+    try {
+        Write-Verbose -Message "Importing class from the script: $scriptName"
+        .   $scriptName
     }
-    
-    process {
-        if (!$OnlyIfExists) {
-            Write-Warning -Message "Adding path $Path without checking for existence!"
-            $tempArray += $Path
-        } else {
-            if (Test-Path -Path $Path) {
-                Write-Verbose -Message "Adding path $Path to selected scope $Scope"
-                $tempArray += $Path
-            } else {
-                Write-Error -Message "Path $Path does not exists! Exiting."
-                break
-            }
-        }
-        Write-Verbose -Message $tempArray
-        $outString = $tempArray -join ";"
+    catch {
+        Write-Warning -Message "Cannot import class from the script: $scriptName"
+        throw   $_
     }
-    
-    end {
-        return $outString
-    }
-}
+})
 
-function Remove-Path {
-    <#
-    .SYNOPSIS
-        Removes path from environment variable.
-    .DESCRIPTION
-        Removes path from environment variable.
-    .EXAMPLE
-        PS C:\> Remove-Path -Path "C:\Path\Does\Not\Exists" -Scope $env:Path
-        Removes path "C:\Path\Does\Not\Exists" from environment variable $env:Path and return string.
-    .INPUTS
-        [System.String]
-    .OUTPUTS
-        [System.String]
-    .NOTES
-        ...
-    #>
-    [CmdletBinding()]
-    param (
-        # Path to remove
-        [Parameter(Mandatory=$true,HelpMessage="Path to remove")]
-        [string]
-        $Path,
+@($importFuncPriv + $importFuncPubl).ForEach({
+    [string]$scriptName =   $_
+    try {
+        Write-Verbose -Message "Importing function from the script: $scriptName"
+        .   $scriptName
+    }
+    catch {
+        Write-Warning -Message "Cannot import function from the script: $scriptName"
+        throw   $_
+    }
+})
 
-        # Scope. Default is $env:Path
-        [Parameter(HelpMessage="Scope. Default is env:Path")]
-        [string]
-        $Scope = $env:Path
-    )
-    
-    begin {
-        [array]$tempArray = Get-Path -Path $Scope
-        [array]$outArray = @()
-        [string]$outString = $null
+[string[]]$aliasesList  =   @()
+$exportFunctions.ForEach({
+    [string]$functionName   =   $_
+    Write-Verbose -Message "Exporting public function: $functionName"
+    Export-ModuleMember -Function $functionName
+    try {
+        Write-Verbose   -Message "Getting alias for the function: $functionName"
+        $aliasesList    +=  (Get-Alias -Definition $functionName -ErrorAction Stop).Name
     }
-    
-    process {
-        foreach ($pathStr in $tempArray) {
-            if (!($pathStr -ne $Path)) {
-                $outArray += $pathStr
-            }
-        }
-        $outString = $outArray -join ";"
+    catch {
+        Write-Verbose   -Message "Unable to get aliases for the function: $functionName"
     }
-    
-    end {
-        return $outString
-    }
-}
-
-Export-ModuleMember -Function @(
-    "Get-Path",
-    "Find-Path",
-    "Add-Path",
-    "Remove-Path"
-    )
+})
+$aliasesList.ForEach({
+    Write-Verbose   -Message "Exporting alias: $_"
+    Export-ModuleMember -Alias $_
+})
